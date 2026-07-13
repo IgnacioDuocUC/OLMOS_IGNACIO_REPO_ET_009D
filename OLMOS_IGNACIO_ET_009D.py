33c8dc66333de822bb2717b60f8ee1d916b05c9a
Ignacio Antonio Olmos <ig.olmosa@duocuc.cl>
2026-07-13 11:38:15 -0400

    Add files via upload
    versión 2

--- OLMOS_IGNACIO_ET_009D.py
+++ OLMOS_IGNACIO_ET_009D.py
@@ -4,6 +4,16 @@ import time
 import sys
 
 #Listas y diccionarios
+#planes = {
+#    'F001': ['nombre_plan': 'Plan Básico', 'tipo': 'mensual', 'duracion_meses': 1, 'acceso_piscina': False, 'incluye_clases': False, 'horario': 'libre'],
+#    'F002': ['nombre_plan': 'Plan Full', 'tipo': 'mensual', 'duracion_meses': 1, 'acceso_piscina': True, 'incluye_clases': True, 'horario': 'libre'],
+#    'F003': ['nombre_plan': 'Plan Estudiante', 'tipo': 'trimestral', 'duracion_meses': 3, 'acceso_piscina': False, 'incluye_clases': True, 'horario': 'tarde'],
+#    'F004': ['nombre_plan': 'Plan Senior', 'tipo': 'trimestral', 'duracion_meses': 3, 'acceso_piscina': True, 'incluye_clases': False, 'horario': 'mañana'],
+#    'F005': ['nombre_plan': 'Plan Anual Pro', 'tipo': 'anual', 'duracion_meses': 12, 'acceso_piscina': True, 'incluye_clases': True, 'horario': 'libre'],
+#    'F006': ['nombre_plan': 'Plan Nocturno', 'tipo': 'mensual', 'duracion_meses': 1, 'acceso_piscina': False, 'incluye_clases': True, 'horario': 'noche']
+#}
+#código sin utilizar, tiene fallos.
+
 planes = {
     'F001': ['Plan Básico', 'mensual', 1, False, False, 'libre'],
     'F002': ['Plan Full', 'mensual', 1, True, True, 'libre'],
@@ -13,13 +23,14 @@ planes = {
     'F006': ['Plan Nocturno', 'mensual', 1, False, True, 'noche'],
 }
 
+
 inscripciones = {
     'F001': [14990, 30],
     'F002': [22990, 10],
     'F003': [39990, 0],
     'F004': [35990, 6],
     'F005': [159990, 2],
-    'F006': [18990, 15],
+    'F006': [18990, 15]
 }
 
 #Funciones
@@ -27,3 +38,9 @@ def LeerOpcion():
     print('========== MENÚ PRINCIPAL ==========')
     print('1. Cupos por tipo de plan.')
     print('2. Búsqueda de planes por rango de precio.')
+    print('3. Actualizar precio de plan.')
+    print('4. Agregar plan.')
+    print('5. Eliminar plan.')
+    print('6. Salir.')
+
+def Cupos_Tipo(tipo):
